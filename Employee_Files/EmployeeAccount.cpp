c36c7707d1dc87d3dd9af29d7067b185341e3553
Brinchiskii <sebastianbrinch.mail@gmail.com>
2023-07-23 16:06:55 +0200

    Updated the EmployeeAccount files. Created a class for EmployeeAccount

--- Employee_Files/EmployeeAccount.cpp
+++ Employee_Files/EmployeeAccount.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "EmployeeAccount.h"
+
+// constructor for EmployeeAccount()
+EmployeeAccount::EmployeeAccount(int acno, std::string fname, std::string lname) {
+    setEmployeeId(acno);
+    setFirstName(fname);
+    setLastName(lname);
+}
+
+void EmployeeAccount::setEmployeeId(int employee_Id) {
+    employeeId = employee_Id;
+}
+
+void EmployeeAccount::setFirstName(std::string first_name) {
+    const char* fn = first_name.data();
+    int len = first_name.size();
+    len = (len < MAX_LENGTH ? len : MAX_LENGTH - 1);
+    strncpy(firstname, fn, len);
+    firstname[len] = '\0';
+}
+
+void EmployeeAccount::setLastName(std::string last_name) {
+    const char* ln = last_name.data();
+    int len = last_name.size();
+    len = (len < MAX_LENGTH ? len : MAX_LENGTH - 1);
+    strncpy(lastname, ln, len);
+    lastname[len] = '\0';
+}
+
+int EmployeeAccount::getEmployeeId() const {
+    return employeeId;
+}
+
+std::string EmployeeAccount::getFirstName() const {
+    return firstname;
+}
+
+std::string EmployeeAccount::getLastName() const {
+    return lastname;
+}
+
+// destructor for EmployeeAccount()
+EmployeeAccount::~EmployeeAccount() = default;
--- Employee_Files/EmployeeAccount.h
+++ Employee_Files/EmployeeAccount.h
@@ -5,4 +5,27 @@
 #ifndef BANKMANAGEMENTSYSTEM_EMPLOYEEACCOUNT_H
 #define BANKMANAGEMENTSYSTEM_EMPLOYEEACCOUNT_H
 
+#include <string>
+
+class EmployeeAccount {
+public:
+    static const int MAX_LENGTH = 30;
+    EmployeeAccount(int = 0, std::string = "", std::string = "");
+    ~EmployeeAccount();
+
+    void setEmployeeId(int);
+    void setFirstName(std::string);
+    void setLastName(std::string);
+
+    int getEmployeeId() const;
+    std::string getFirstName() const;
+    std::string getLastName() const;
+
+private:
+    int employeeId{};
+    char firstname[MAX_LENGTH]{};
+    char lastname[MAX_LENGTH]{};
+};
+
+
 #endif //BANKMANAGEMENTSYSTEM_EMPLOYEEACCOUNT_H
